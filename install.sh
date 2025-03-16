9eedac29ec94a6395738d04cf8be39bb4c5f176a
人間工作 <coolxiaom95@gmail.com>
2025-03-16 19:27:36 +0800

    upload spigot javadoc

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# == 0 ]; then
+  EXPORT_DIR="./out"
+else 
+  EXPORT_DIR="$*"
+fi
+echo "output dir: $EXPORT_DIR"
+
+for file in ./spigot/*.zip; do
+  if [ -f "$file" ]; then
+    version=$(basename "${file%.*}")
+    output="$EXPORT_DIR/spigot/$version"
+    mkdir -p $output
+    echo "unzip $file -> $output"
+    unzip -q $file -d $output
+  fi
+done
