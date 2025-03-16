27b16843d5c4ce166e8081d2928d17e284130274
人間工作 <coolxiaom95@gmail.com>
2025-03-16 20:14:08 +0800

    upload paper javadoc

--- install.sh
+++ install.sh
@@ -16,3 +16,13 @@ for file in ./spigot/*.zip; do
     unzip -q $file -d $output
   fi
 done
+
+for file in ./paper/*.zip; do
+  if [ -f "$file" ]; then
+    version=$(basename "${file%.*}")
+    output="$EXPORT_DIR/paper/$version"
+    mkdir -p $output
+    echo "unzip $file -> $output"
+    unzip -q $file -d $output
+  fi
+done
